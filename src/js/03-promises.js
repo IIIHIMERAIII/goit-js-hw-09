6d756f2c8d88e232fbc4e879116d1fa1a8ea1702
Maksym <PTNPNH722@gmail.com>
2022-11-02 16:21:46 +0200

    final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from "notiflix";
+const formRef = document.querySelector(".form");
+
+formRef.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  let delayInput = Number(formRef.elements.delay.value);
+  let stepInput = Number(formRef.elements.step.value);
+  
+  for (let amount = 0; amount < Number(formRef.elements.amount.value); amount++)
+    createPromise(amount +1, (delayInput += stepInput))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
 }
+
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, (delay -= Number(formRef.elements.step.value)));
+    });
+  }
+
+
+
+
+
+
